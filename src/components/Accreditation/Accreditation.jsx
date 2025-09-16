967bd6b5015d84a535d1fcc9b922db0ac95384b5
Ahmed-Deraz <ahmed.alaa.deraaz@gmail.com>
2025-09-16 15:56:54 +0300

    Fix marquee loop and mobile display issue

--- src/components/Accreditation/Accreditation.jsx
+++ src/components/Accreditation/Accreditation.jsx
@@ -5,8 +5,15 @@ const Accreditation = () => {
   return (
     <div className="marquee-container">
       <div className="marquee-track">
-        {[...accreditationLogos, ...accreditationLogos].map((logo, index) => (
+        {accreditationLogos.map((logo, index) => (
           <div className="marquee-slide" key={index}>
+            <img className="marquee-img" src={logo} alt={`Logo ${index}`} />
+          </div>
+        ))}
+      </div>
+      <div className="marquee-track">
+        {accreditationLogos.map((logo, index) => (
+          <div className="marquee-slide" key={`dup-${index}`}>
             <img className="marquee-img" src={logo} alt={`Logo ${index}`} />
           </div>
         ))}
